0d4b5d76f2c546ad33d6b81ce1afbd1ad9f82f6d
discompie <discompie@gmail.com>
2025-08-21 00:10:39 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-// 이 파일에 JavaScript 코드를 작성할 수 있습니다.
-// 예를 들어, 경고창을 띄우는 코드를 추가해 볼까요?
-// alert("안녕하세요, 나의 첫 웹사이트에 오신 것을 환영합니다!");
+document.addEventListener('DOMContentLoaded', () => {
+    const otterContainer = document.querySelector('.otter-container');
+    
+    // 귀여운 애니메이션 수달 이미지 URL 목록
+    const otterImages = [
+        'https://www.flaticon.com/svg/static/icons/svg/1963/1963282.svg', // 수달1
+        'https://www.flaticon.com/svg/static/icons/svg/1963/1963273.svg', // 수달2
+        'https://www.flaticon.com/svg/static/icons/svg/1963/1963261.svg'  // 수달3
+    ];
+
+    // 수달을 추가하고 클릭 이벤트를 등록하는 함수
+    const createOtter = (imageUrl) => {
+        const otter = document.createElement('img');
+        otter.src = imageUrl;
+        otter.classList.add('otter');
+        otter.alt = '귀여운 수달';
+        otterContainer.appendChild(otter);
+
+        otter.addEventListener('click', () => {
+            const messages = [
+                '안녕!',
+                '나를 클릭했구나!',
+                '헤헤헷',
+                '사랑해!',
+                '날 보러 와줘서 고마워!'
+            ];
+            const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+            alert(randomMessage);
+        });
+    };
+
+    // 웹사이트에 3마리의 수달을 추가
+    otterImages.forEach(imageUrl => {
+        createOtter(imageUrl);
+    });
+});
